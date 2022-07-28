1c21e3efedb7ca28703792fc1c2bac6ae9f49d87
khantminht <khantminht.bizs@gmail.com>
2022-07-28 21:17:17 +0630

    28july9:00pm

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
 $(document).ready(function(){
-    
+
+    // Start Banner Section 
+    $(".carousel").carousel({
+        interval:2000
+    });
+    // End Banner Section 
 
 
+    // Start Info SEction
+    $(window).scroll(function(){
+        let getscrollpoint = $(this).scrollTop();
+
+        // console.log(getscrollpoint);
+
+        if( getscrollpoint >= 420){
+            $(".infotexts").addClass("fromlefts");
+            $(".infopics").addClass("fromrights");
+        }else{
+            $(".infotexts").removeClass("fromlefts");
+            $(".infopics").removeClass("fromrights");
+        }
+    })
+    // End Info SEction
 
 
 
